a05cd9622bcc804626b71ab1b4cc4abfda9e4a1c
bdiwy <deve.ahmed.bdiwy@gmail.com>
2026-06-28 05:22:10 +0300

    add some configration for swagger auth

--- Api/Program.cs
+++ Api/Program.cs
@@ -75,6 +75,31 @@ builder.Services.AddSwaggerGen(options =>
         Version = "v1",
         Description = "InvoiceHub HTTP API"
     });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter your JWT token in the format: Bearer {token}"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 
